bce61cac039b7a499294971151d6db287c9170a0
Tatiana Karabut <tyulalex@gmail.com>
2017-12-10 22:56:31 +0300

    check_sites_health

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # Sites Monitoring Utility
 
+script takes a txt file with urls as an input and check:
+ - site responds with 200
+ - domain expired more than in 30 days
+
+ urls in txt file shall be divided with \n
+ 
+
+# System requirements
+required python 3.5 installed
+pip install -r requirements.txt
+
+# How to install and run
+
+to run it: 
+on windows:
+```
+    python check_sites_health.py --path <path_to_file_with_urls>
+```
+on linux might require 
+```
+  python3 check_sites_health.py --path <path_to_file_with_urls>
+  ```
+--path shall be in the format of your system
+
+Supported Systems: Windows, Unix
 [TODO. There will be project description]
 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,60 @@
+import argparse
+import requests
+import whois
+from datetime import datetime
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p', '--path',
+        help='path to file with urls')
+    return parser.parse_args()
+
+
 def load_urls4check(path):
-    pass
+    with open(path) as url_file:
+        return url_file.read().splitlines()
+
 
 def is_server_respond_with_200(url):
-    pass
+    response = requests.get(url, verify=False)
+    if response.status_code == 200:
+        return True
+    return False
+
+
+def is_domain_expired_in_more_than_1_month(domain_name):
+    expiration_date = get_domain_expiration_date(domain_name)
+    now = datetime.now()
+    return (expiration_date - now).days > 30
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois.whois(domain_name)
+    if type(domain_info.expiration_date) is list:
+        return domain_info.expiration_date[1]
+    return domain_info.expiration_date
+
+
+def collect_errors(url):
+    errors = []
+    if not is_server_respond_with_200(url):
+        errors.append(
+            'ERROR: HEALTHCHECK STATUS is not 200'.format(url))
+    if not is_domain_expired_in_more_than_1_month(url):
+        errors.append(
+            'WARNING: domain expired in less than 30 days')
+    return errors
+
 
 if __name__ == '__main__':
-    pass
+    path = parse_args().path
+    urls = load_urls4check(path)
+    for url in urls:
+        errors = collect_errors(url)
+        if errors:
+            for error in errors:
+                print(error)
+        else:
+            print('HEALTHCHECK for {} is OK'.format(url))
